75267be87f51a4ffd9d8e4ee71beb30db30c16f7
Vitaliy Timchenko <vitaliy.timchenko@gmail.com>
2016-01-11 02:53:14 +0200

    first commit

---
+++ js/coffee.js
@@ -0,0 +1,98 @@
+var timer,
+    timerId = 0,
+    seconds = 0,
+    tableHistory,
+    tableTop;
+
+$(document).ready(function(){
+    timer = $("#timerText");
+    tableHistory = $("#history>tbody");
+    tableTop = $("#top>tbody");
+	makeRequest(seconds);
+
+    $("button#btnStart").on("click", function(){
+        var buttonText = $(this).html();
+
+        if (buttonText === "Start") {
+            $(this).html("Stop");
+            timerId = setInterval(changeText, 100);
+        } else {
+            $(this).html("Start");
+            window.clearInterval(timerId);
+            makeRequest(seconds);
+            seconds = 0;
+        }
+    });
+});
+
+function showLoading(){
+    tableHistory.html('<tr><td colspan="2"><img src="img/loading.gif"></td></tr>').css("text-align", "center");
+    tableTop.html('<tr><td colspan="2"><img src="img/loading.gif"></td></tr>').css("text-align", "center");
+}
+
+/**
+* if seconds == 0 then get data only
+* if seconds > 0 get data and save seconds to DB
+*/
+function makeRequest(seconds){
+    showLoading();
+    $.ajax({
+        url: "php/coffee.php",
+        method: "post",
+        dataType: "json",
+        data: {timer: seconds},
+        success: function(data){
+            var html = "No data available...";
+            data.records.forEach(function(item, i){
+                html += "<tr><td>" + item["date"] + "</td><td>" +  secToHHMMSS(item["seconds"])  + "</td></tr>";
+            });
+            tableHistory.html(html);
+
+            html = "No data available...";
+            data.top.forEach(function(item, i){
+                html += "<tr><td>" + item["date"] + "</td><td>" + secToHHMMSS(item["seconds"]) + "</td></tr>";
+            });
+            tableTop.html(html);
+        },
+        error: function (){
+            var errorText = '<tr><td  colspan="2">Sorry, something went wrong...</td></tr>';
+            tableHistory.html(errorText).css("text-align", "center");
+            tableTop.html(errorText).css("text-align", "center");
+        },
+    });
+}
+
+function changeText(){
+    seconds++;
+    timer.html(secToHHMMSS(seconds, true));
+}
+
+function secToHHMMSS(seconds, animate){
+    var msec = parseInt(seconds, 10);
+    var sec = Math.floor(msec / 10);
+
+    var hh = Math.floor(sec / 3600);
+    var mm = Math.floor((sec - hh * 3600) / 60);
+    var ss = Math.floor(sec - hh * 3600 - mm * 60);
+    var zz =  msec - hh * 36000 - mm * 600 - ss * 10;
+
+    if (animate === true) {
+        var delimiter = (ss % 2 === 0) ? ":" : " ";
+    } else {
+        var delimiter = ":";
+    }
+
+    if (hh < 10) {
+        hh = '0' + hh;
+    }
+
+    if (mm < 10) {
+        mm = '0' + mm;
+    }
+
+    if (ss < 10) {
+        ss = '0' + ss;
+    }
+
+    return hh + delimiter + mm + delimiter + ss + "." + zz;
+}
---
+++ php/coffee.php
@@ -0,0 +1,82 @@
+<?php
+ini_set('display_errors', 0);
+error_reporting(E_ALL);
+
+header('Content-Type: application/json');
+
+require_once(__DIR__.'/db_coffee_config.php');
+
+if (!isset($_POST['timer'])) {
+    header("HTTP/1.0 400 Bad Request. There are no required POST parameter.");
+    exit;
+}
+
+$seconds = (int)$_POST["timer"];
+
+define('TIMEZONE', 'Europe/Kiev');
+date_default_timezone_set(TIMEZONE);
+
+// to set timezone in mysql: SET time_zone='offset';
+// where offset is a string value representing the difference to UTC/GMT,
+// e.g. ‘-4:00′, ‘+3:00′, ‘+10:30′, etc. Note that the +/- sign is essential
+// — even for zero — and timezone offsets are not necessarily a whole hour.
+
+// create a new DateTime object, find the offset in seconds, and convert it to minutes
+$now = new DateTime();
+$mins = $now->getOffset() / 60;
+
+// We can now calculate whole hours and minutes. The first line determines whether the offset is positive or negative,
+// then converts the value to a positive number to make the calculation easier
+$sgn = ($mins < 0 ? -1 : 1);
+$mins = abs($mins);
+$hrs = floor($mins / 60);
+$mins -= $hrs * 60;
+
+$offset = sprintf('%+d:%02d', $hrs*$sgn, $mins);
+
+try {
+    $dsn = "mysql:host=$host;dbname=$database;charset=utf8";
+    $opt = array(
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    );
+    $db = new PDO($dsn, $user, $pass, $opt);
+    $db->exec("SET time_zone='$offset';");
+
+    // add new record
+    if ($seconds > 0) {
+        $sql = "INSERT INTO `". $table ."` (seconds) VALUES (:seconds)";
+        $stmt = $db->prepare($sql);
+        $stmt->bindValue(":seconds", $seconds);
+        $stmt->execute();
+    }
+
+    // records list
+    $sql = "SELECT DATE_FORMAT(`date`, '%Y.%m.%d %H:%i') AS `date`, `seconds` FROM `". $table ."` ORDER BY `date` DESC LIMIT 10";
+    $stmt = $db->prepare($sql);
+    $stmt->execute();
+
+    $records = $stmt->fetchAll();
+
+    // top result
+    $stmt = $db->prepare("SELECT DATE_FORMAT(`date`, '%Y.%m.%d %H:%i') AS `date`, `seconds` FROM `". $table ."` ORDER BY `seconds` DESC LIMIT 3");
+    $stmt->execute();
+    $top = $stmt->fetchAll();
+    // print_r($top); exit();
+    $return = array();
+    
+    if (isset($top)) {
+        $return["top"] = $top;
+    }
+
+    if (isset($records)) {
+        $return["records"] = $records;
+    }
+
+    echo json_encode($return);
+
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
+
+$db = null;
